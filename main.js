60bdfae24dac1e7696818b3e983c94412fbaa001
mikechel <michekmi@hotmail.com>
2023-06-15 09:12:17 -0300

    Script
    Adicionado a logica

---
+++ main.js
@@ -0,0 +1,20 @@
+const data = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+
+const today = new Date();
+console.log(today);
+
+const weekDays = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado"]
+
+const AllMonths = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembreo"]
+
+
+
+
+date.innerHTML = today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = AllMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
